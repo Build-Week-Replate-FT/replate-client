94955e688b4a28d53777fbd418e2f1d1b93e6fbf
Nattajohn Rojanasupya <n.rojanasupya@gmail.com>
2019-10-21 16:42:18 -0700

    styling with material-ui

--- src/components/BusinessSignUp.js
+++ src/components/BusinessSignUp.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import useForm from 'react-hook-form'
+import Paper from '@material-ui/core/Paper';
 
 export function BusinessSignUp() {
     const { register, handleSubmit, errors } = useForm();
     const onSubmit = data => console.log(data);
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder="Business Name" name="Name" ref={register({required: true, maxLength: 100})} />
-            <input type="email" placeholder="Email" name="Email" ref={register({required: true})} />
-            <input type="password" placeholder="Password" name="Password" ref={register({required: true, min: 6})} />
-            <input type="text" placeholder="Address" name="Address" ref={register({required: true, min: 6})} />
-            <input type="text" placeholder="City" name="City" ref={register({required: true, min: 6})} />
-            <input type="text" placeholder="State" name="State" ref={register({required: true, min: 6})} />
-            <input type="text" placeholder="Zip" name="Zip" ref={register({required: true, min: 6})} />
-            <input type="text" placeholder="Website" name="Website" ref={register({required: true, min: 6})} />
+        <Paper>
+            <h1>Business Sign Up</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" placeholder="Business Name" name="Name" ref={register({required: true, maxLength: 100})} />
+                <input type="email" placeholder="Email" name="Email" ref={register({required: true})} />
+                <input type="password" placeholder="Password" name="Password" ref={register({required: true, min: 6})} />
+                <input type="text" placeholder="Address" name="Address" ref={register({required: true, min: 6})} />
+                <input type="text" placeholder="City" name="City" ref={register({required: true, min: 6})} />
+                <input type="text" placeholder="State" name="State" ref={register({required: true, min: 6})} />
+                <input type="text" placeholder="Zip" name="Zip" ref={register({required: true, min: 6})} />
+                <input type="text" placeholder="Website" name="Website" ref={register({required: true, min: 6})} />
 
-            <button type="submit">Sign Up</button>
-        </form>
-    );
+                <button type="submit">Sign Up</button>
+            </form>
+        </Paper>
+   );
 }
--- src/components/VolunteerSignUp.js
+++ src/components/VolunteerSignUp.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import useForm from 'react-hook-form'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField'
+
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  dense: {
+    marginTop: theme.spacing(2),
+  },
+  menu: {
+    width: 200,
+  },
+}));
 
 export function VolunteerSignUp() {
-    const { register, handleSubmit, errors } = useForm();
-    const onSubmit = data => console.log(data);
+	const { register, handleSubmit, errors } = useForm();
+	const classes = useStyles();
+  const onSubmit = data => console.log(data);
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder="Name" name="Name" ref={register({required: true, maxLength: 100})} />
-            <input type="email" placeholder="Email" name="Email" ref={register({required: true})} />
-            <input type="password" placeholder="Password" name="Password" ref={register({required: true, min: 6})} />
-
-            <button type="submit" >Sign Up</button>
-        </form>
-    );
+  return (
+    <Paper>
+			<h1>Volunteer Sign Up</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+				<TextField
+					label="Name"
+					className={classes.textField}
+					// value={values.name}
+					// onChange={handleChange('name')}
+					margin="normal"
+					variant="outlined"
+					innerRef={register({required: true, maxLength: 100})}
+      	/>
+ 				<TextField
+					label="Email"
+					className={classes.textField}
+					type='email'
+					margin="normal"
+					variant="outlined"
+					innerRef={register({required: true})}
+      	/>
+ 				<TextField
+					label="Password"
+					className={classes.textField}
+					type='password'
+					margin="normal"
+					variant="outlined"
+					innerRef={register({required: true, min: 6})}
+      	/>
+        {/* <input type="text" placeholder="Name" name="Name" ref={register({required: true, maxLength: 100})} /> */}
+        {/* <input type="email" placeholder="Email" name="Email" ref={register({required: true})} />
+        <input type="password" placeholder="Password" name="Password" ref={register({required: true, min: 6})} /> */}
+        <button type="submit" >Sign Up</button>
+      </form>
+    </Paper> 
+   );
 }
