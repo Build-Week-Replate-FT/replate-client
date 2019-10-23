545e60dfa849519e8dff26fde066ec54f5b910e7
Joe <josephc.lambert@gmail.com>
2019-10-22 23:12:14 -0700

    navbar renders login instead of profile if user is anonymous

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,15 +10,22 @@ const StyledNavWrapper = styled.div`
   padding: 0 2%;
 `;
 
+const user = {
+  // role: "business"
+};
+
 export const Navbar = props => {
+  const NavLink = user.role ? (
+    <Link to="/profile">Profile</Link>
+  ) : (
+    <Link to="/login">Login</Link>
+  );
   return (
     <StyledNavWrapper>
       <h1>
         <Link to="/">Replate</Link>
       </h1>
-      <nav>
-        <Link to="/profile">Profile</Link>
-      </nav>
+      <nav>{NavLink}</nav>
     </StyledNavWrapper>
   );
 };
