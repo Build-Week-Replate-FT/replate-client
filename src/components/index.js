b579d85ff004189c0e977a192835928bdd3a5e74
Nattajohn Rojanasupya <n.rojanasupya@gmail.com>
2019-10-23 11:40:38 -0700

    added volunteer dashboard routes and basic layout

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export { LandingPage } from './LandingPage';
 export { BusinessSignUp } from './BusinessSignUp';
 export { VolunteerSignUp } from './VolunteerSignUp';
 export { BusinessDashboard } from './BusinessDashboard';
+export { VolunteerDashboard } from './VolunteerDashboard';
 export { Navbar } from './Navbar';
