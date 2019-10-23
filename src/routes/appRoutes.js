6bbff3b1157eb209dd6967241b29b4816aade888
Joe <josephc.lambert@gmail.com>
2019-10-23 08:11:14 -0700

    removes hardcoded user

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,15 +1,11 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { PublicRoutes } from "./publicRoutes";
-import { AuthenticatedRoutes } from "./authenticatedRoutes";
-import { Navbar } from "../components";
-
-const user = {
-  // role: "business"
-};
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { PublicRoutes } from './publicRoutes';
+import { AuthenticatedRoutes } from './authenticatedRoutes';
+import { Navbar } from '../components';
 
 export function AppRoutes() {
-  // const { user } = useSelector(state => state.authentication);
+  const { user } = useSelector(state => state.authentication);
   return (
     <>
       <Navbar />
