cd2024de2912dd32c7ad9f8542d46084dcfb2815
Joe <josephc.lambert@gmail.com>
2019-10-23 12:25:07 -0700

    login sets token to localStorage. there is no user info returned. so we're waiting on that now.

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { getToken } from '../api';
 
 export function axiosWithAuth(baseURL) {
-  const token = getToken() || btoa('lambda-client:lambda-secret');
+  const token = getToken() ? `Bearer ${getToken()}` : `Basic ${btoa('lambda-client:lambda-secret')}`;
   const client = axios.create({
     baseURL,
     headers: {
-      Authorization: `Basic bGFtYmRhLWNsaWVudDpsYW1iZGEtc2VjcmV0`,
+      Authorization: token,
       'Content-Type': 'application/x-www-form-urlencoded',
     },
   });
