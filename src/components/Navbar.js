b08c4f4ba32ad4efae0e2001042b0bd90fd13482
Nattajohn Rojanasupya <n.rojanasupya@gmail.com>
2019-10-23 23:38:54 -0700

    added styling to dashboards and schedule form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,7 @@ import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { authActionCreators } from '../actions';
+import { fontFamily } from '@material-ui/system';
 
 const StyledNavWrapper = styled.div`
   display: flex;
@@ -31,7 +32,8 @@ const useStyles = makeStyles(theme => ({
     textDecoration: 'none'
   },
   button: {
-    color: '#23293B'
+    color: '#23293B',
+    fontFamily: 'inherit'
   }
 }));
 
