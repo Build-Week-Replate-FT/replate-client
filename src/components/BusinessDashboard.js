bbcbf33bbae2de5b8ed6d9aa93cf6b1914319d2d
Nattajohn Rojanasupya <n.rojanasupya@gmail.com>
2019-10-23 23:59:15 -0700

    added css grid to business dashboard

--- src/components/BusinessDashboard.js
+++ src/components/BusinessDashboard.js
@@ -25,6 +25,16 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     alignItems: 'center',
   },
+  wrapper: {
+    display: 'grid',
+    gridTemplateRows: 'repeat(2, 50%)',
+    gridGap: '10px'
+  },
+  paper: {
+    padding: '10px',
+    width: '100%',
+    backgroundColor: 'aliceblue'
+  }
 }));
 
 export function BusinessDashboard() {
@@ -50,43 +60,45 @@ export function BusinessDashboard() {
   }
 
   return (
-    <Container>
-      <h1>Business Dashboard</h1>
-      {scheduling && <SchedulePickupForm scheduling={scheduling} setScheduling={setScheduling}/>}
-      <Box>
-        <Paper>
-          <h4>Office Location</h4>
-          <p>{user.address}</p>
-          <p>{user.city}, {user.state} {user.zip}</p>
-        </Paper>
-      </Box>
+    <>
+      <Container className={classes.wrapper}>
+        {scheduling && <SchedulePickupForm scheduling={scheduling} setScheduling={setScheduling}/>}
+        <Box>
+          <h1>Business Dashboard</h1>
+          <Paper className={classes.paper}>
+            <h2>Office Location</h2>
+            <p>{user.address}</p>
+            <p>{user.city}, {user.state} {user.zip}</p>
+          </Paper>
+        </Box>
 
-      <Box>
-        <Paper>
-          <Box>
-            <Box className={classes.makeDonationBox}>
-              <h2>Request a Pickup</h2>
-              <Fab disabled={scheduling} onClick={schedulePickup} color='primary' size='small' aria-label='add' className={classes.fab}>
-                <AddIcon />
-              </Fab>
+        <Box>
+          <Paper className={classes.paper}>
+            <Box>
+              <Box className={classes.makeDonationBox}>
+                <h2>Request a Pickup</h2>
+                <Fab disabled={scheduling} onClick={schedulePickup} color='primary' size='small' aria-label='add' className={classes.fab}>
+                  <AddIcon />
+                </Fab>
+              </Box>
+              <h4>Scheduled Pickups</h4>
+              <Grid container spacing={2}>
+                {pickups.map(pickup => (
+                  <Grid item key={pickup.pickupid}>
+                    <Card>
+                      <CardContent>
+                        <h2>{pickup.foodtype}</h2>
+                        <p>{pickup.quantity} {pickup.quantityunit}</p>
+                        <p>Pickup Time: {new Date(pickup.postdate).toDateString()}</p>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
             </Box>
-            <h4>Scheduled Pickups</h4>
-            <Grid container spacing={2}>
-              {pickups.map(pickup => (
-                <Grid item key={pickup.pickupid}>
-                  <Card>
-                    <CardContent>
-                      <h2>{pickup.foodtype}</h2>
-                      <p>{pickup.quantity} {pickup.quantityunit}</p>
-                      <p>Pickup Time: {new Date(pickup.postdate).toDateString()}</p>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
-          </Box>
-        </Paper>
-      </Box>
-    </Container>
+          </Paper>
+        </Box>
+      </Container>
+    </>
   );
 }
